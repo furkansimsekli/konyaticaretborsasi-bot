ae47f1c148cb5c8703656d4cc8168a4d9d8f398a
furkansimsekli <simseklifurkan0@gmail.com>
2023-08-21 16:15:30 +0300

    config: integrate toml
    Resolves #2

--- src/config.py
+++ src/config.py
@@ -1,20 +1,27 @@
+import sys
+
+import toml
+
+config_path = sys.argv[1] if len(sys.argv) > 1 else 'config.toml'
+config = toml.load(config_path)
+
 # Telegram Bot Token
-TELEGRAM_API_KEY: str = ''
+TELEGRAM_API_KEY: str = config['TELEGRAM_API_KEY']
 
 # MongoDB Connection String
-DB_STRING: str = ''
+DB_STRING: str = config['DB_STRING']
 
 # Admin Chat ID
-ADMIN_ID: int = 0
+ADMIN_ID: int = config['ADMIN_ID']
 
 # Logger Chat ID
-LOGGER_CHAT_ID: int = 0
+LOGGER_CHAT_ID: int = config['LOGGER_CHAT_ID']
 
 # Time Configurations for the Price Checking Task
-PRICE_CHECK_HOURS: list[int] = []
-PRICE_CHECK_MINUTES: list[int] = []
+PRICE_CHECK_HOURS: list[int] = config['PRICE_CHECK_HOURS']
+PRICE_CHECK_MINUTES: list[int] = config['PRICE_CHECK_MINUTES']
 
 # Polling or Webhook?
-WEBHOOK_CONNECTED: bool = False
-PORT: str = ''
-WEBHOOK_URL: str = '' + TELEGRAM_API_KEY
+WEBHOOK_CONNECTED: bool = config['WEBHOOK_CONNECTED']
+PORT: str = config['PORT']
+WEBHOOK_URL: str = config['WEBHOOK_URL']
